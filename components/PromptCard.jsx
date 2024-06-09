5cddd1011b6892d5769bcd263f9cd98b5c14b512
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-06-09 20:51:54 +0530

    Read feature implemented

---
+++ components/PromptCard.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+import { TbCopyCheck } from "react-icons/tb";
+import { TbCopy } from "react-icons/tb";
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+
+    setTimeout(() => setCopied(""), 10000);
+  };
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user-image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900 capitalize">
+              {post.creator.userName}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className="copy_btn" onClick={handleCopy}>
+          {copied == post.prompt ? (
+            <TbCopyCheck size={18} />
+          ) : (
+            <TbCopy size={18} />
+          )}
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-500">{post.prompt}</p>
+      <p
+        className="blue_gradient font-inter text-sm cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
+};
+
+export default PromptCard;
