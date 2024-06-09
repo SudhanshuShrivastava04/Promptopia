b33d4dd31a856a09b784d27095523e7195a3e9d3
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-06-10 03:08:05 +0530

    disable eslint in build

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { Suspense } from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+
 import Form from "@/components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
-  const [submitting, setsubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
-    const getPromptsDetails = async () => {
-      const response = await fetch(`api/prompt/${promptId}`);
-
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
 
       setPost({
@@ -27,14 +24,14 @@ const EditPrompt = () => {
       });
     };
 
-    if (promptId) getPromptsDetails();
+    if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
-    setsubmitting(true);
+    setIsSubmitting(true);
 
-    if (!promptId) return alert("Prompt ID is not found!");
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -51,31 +48,19 @@ const EditPrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setsubmitting(false);
+      setIsSubmitting(false);
     }
   };
 
   return (
-    <Suspense
-      fallback={
-        <Image
-          src="/loader.svg"
-          width={50}
-          height={50}
-          alt="loader"
-          className="object-contain"
-        />
-      }
-    >
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </Suspense>
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
